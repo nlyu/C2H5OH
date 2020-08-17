165f98dc1c6d6a84a8d9f03b19ee39e66f78ee46
nulyu <nulyu@microsoft.com>
2020-08-16 17:33:44 -0700

    add background service, static simple web pages

---
+++ C2H5OH/Data/AlcoholDataLoadHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace C2H5OH.Data
+{
+    internal static class AlcoholDataLoadHelper
+    {
+        /*
+         * Credit to https://immortalcoder.blogspot.com/2013/12/convert-csv-file-to-datatable-in-c.html
+         */
+        public static DataTable ConvertCSVtoDataTable(string strFilePath)
+        {
+            StreamReader sr = new StreamReader(strFilePath, Encoding.UTF8);
+            string[] headers = sr.ReadLine().Split(',');
+            DataTable dt = new DataTable();
+            foreach (string header in headers)
+            {
+                dt.Columns.Add(header);
+            }
+            while (!sr.EndOfStream)
+            {
+                string[] rows = Regex.Split(sr.ReadLine(), ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                DataRow dr = dt.NewRow();
+                for (int i = 0; i < headers.Length; i++)
+                {
+                    dr[i] = rows[i];
+                }
+                dt.Rows.Add(dr);
+            }
+            return dt;
+        }
+    }
+}
---
+++ C2H5OH/Data/AlcoholDataLoadService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace C2H5OH.Data
+{
+    internal class AlcoholDataLoadService: IHostedService
+    {
+        private readonly ILogger logger;
+        private readonly int time;
+        private readonly string basePath = System.Environment.CurrentDirectory;
+        private readonly string tablePath = "/Data/Tables/";
+        private readonly string allTable = "All.csv";
+        private readonly string labelsTable = "Labels.csv";
+        private readonly string mapsTable = "Maps.csv";
+
+        public AlcoholDataLoadService(ILogger<AlcoholDataLoadService> logger)
+        {
+            this.logger = logger;
+            this.time = 10000; 
+        }
+
+        public async  Task StartAsync(CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                DataTable allTableData;
+                var targetTablePath = basePath + tablePath + allTable;
+                try
+                {
+                    allTableData = AlcoholDataLoadHelper.ConvertCSVtoDataTable(targetTablePath);
+                }
+                catch(Exception ex)
+                {
+                    this.logger.LogError($"C2H5OH: Error when loading talbe from {targetTablePath}. Ex msg: {ex.Message}.");
+                }
+
+                this.logger.LogInformation($"C2H5OH: Drinking alcohol for every {this.time}ms.");
+                await Task.Delay(10000, cancellationToken);
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
